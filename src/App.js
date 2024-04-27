43e107bf10594a25999933451bb873a08753a8f1
Joshua989 <vjiu>
2024-04-28 00:13:22 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar  from './component/navbar/Navbar'
 import Footer from './contianers/footer/Footer'
 import Header from './contianers/header/Header'
 import Main from './contianers/main/Main'
-import Feature2 from '././component/././component/feature2';
 import "./App.css"
 
 
@@ -13,7 +12,7 @@ const App = () => {
         <div className='Bg'>
         <Header />
         </div>
-        <Feature2 />
+        <Main />
         <Footer />
        
     </div>
