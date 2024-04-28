24a76cd7270b534236dc25ce69c3c4d121fbc4cc
joshua989 <omolafejoshua25@gmail.com>
2024-04-28 11:22:09 -0700

    hgih

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar  from './component/navbar/Navbar'
 import Footer from './contianers/footer/Footer'
 import Header from './contianers/header/Header'
+import Feature2 from './component/feature2/Feature2'
 import "./App.css"
 
 
@@ -10,6 +11,7 @@ const App = () => {
     <div className='App'>
         <div className='Bg'>
         <Header />
+        <Feature2 />
         </div>
         <Footer />
        
